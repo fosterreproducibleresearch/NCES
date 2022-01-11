bb8c02bba771370b5205f9e140c797f95f769241
NCES <jeankouagou556e@gmail.com>
2022-01-11 23:21:49 +0100

    scripts for running ecii are added

--- Method/dllearner/reproduce_dllearner_experiment_all_kgs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-python ./scripts/reproduce_dllearner_experiment_carcinogenesis_kg.py
-python ./scripts/reproduce_dllearner_experiment_mutagenesis_kg.py
-python ./scripts/reproduce_dllearner_experiment_semantic_bible_kg.py
-python ./scripts/reproduce_dllearner_experiment_family_benchmark_kg.py
